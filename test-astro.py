4fce820698ccf5ed1e980a7d0cdf3236a34542e0
ZakirG <zakirgowani@gmail.com>
2014-05-02 14:16:37 -0500

    Wrote light curve plotter and frame neighbor finder

--- test-astro.py
+++ test-astro.py
@@ -7,13 +7,13 @@ import scipy
 import ds9
 import subprocess
 import numpy as np
-import pylab as pl
-import sklearn.cluster as sk
-from sklearn import metrics
-from sklearn.cluster import KMeans
-from sklearn.datasets import load_digits
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import scale
+import matplotlib.pyplot as plt
+#import sklearn.cluster as sk
+#from sklearn import metrics
+#from sklearn.cluster import KMeans
+#from sklearn.datasets import load_digits
+#from sklearn.decomposition import PCA
+#from sklearn.preprocessing import scale
 
 NUM_ROWS = 32
 NUM_COLS = 32
@@ -44,57 +44,117 @@ def brightest_pixel(frame):
 				max_ind = (i, j)
 	return max_ind
 
+#Find 2x2 area with greatest total luminosity
+#Return index of brightest pixel in the 2x2 area
+def brightest_region(frame):
+	N = NUM_ROWS
+	M = NUM_COLS
+	# Since we're looking for 2x2 regions using the top left index, 
+	# we will only search N-1 rows and M - 1 columns.
+	max_lum = 0
+	best_ind = (0, 0) 
+	for i in range(N - 1):
+		for j in range(M - 1):
+			pix_A = frame[i, j]
+			pix_B = frame[i + 1, j]
+			pix_C = frame[i, j + 1]
+			pix_D = frame[i + 1, j + 1]
+			z = [pix_A, pix_B, pix_D, pix_D]
+			total_lum = sum(z)
+			if total_lum > max_lum:
+				max_lum = total_lum
+				if pix_A == max(z):
+					best_ind = (i, j)
+				if pix_B == max(z):
+					best_ind = (i + 1, j)
+				if pix_C == max(z):
+					best_ind = (i, j + 1)
+				else:
+					best_ind = (i + 1, j + 1)
+	return best_ind
+
+#Returns up to 8 neighbors of a pixel at radius 1 
+#Handles edge cases silently
+#Gives neighbors in a square, NOT a circle
 def frame_neighbors(frame, index_tuple):
 	i = index_tuple[0]
 	j = index_tuple[1]
-	if i != 0 and j != 0 and i != (NUM_ROWS - 1) and j != (NUM_COLS - 1):
-		A = frame[i - 1, j - 1]
-		B = frame[i, j - 1]
-		C = frame[i + 1, j - 1]
-		D = frame[i - 1, j]
-		E = frame[i + 1, j]
-		F = frame[i - 1, j + 1]
-		G = frame[i, j + 1]
-		H = frame[i + 1, j + 1]
+	min_bd = 0
+	level = 1
+	left_bd = NUM_ROWS - 1
+	right_bd = NUM_COLS - 1
+	#Handling the twelve standard edge cases for a grid
+	if i != min_bd and j != min_bd and i != left_bd and j != right_bd:
+		A = frame[i - level, j - level]
+		B = frame[i, j - level]
+		C = frame[i + level, j - level]
+		D = frame[i - level, j]
+		E = frame[i + level, j]
+		F = frame[i - level, j + level]
+		G = frame[i, j + level]
+		H = frame[i + level, j + level]
 		return [A, B, C, D, E, F, G, H]
-	elif i == 0 and j != 0 and i != (NUM_ROWS - 1) and j != (NUM_COLS - 1):
-		B = frame[i, j - 1]
-		C = frame[i + 1, j - 1]
-		D = frame[i + 1, j]
-		E = frame[i, j + 1]
-		F = frame[i + 1, j + 1]
+	elif i == min_bd and j != min_bd and i != left_bd and j != right_bd:
+		B = frame[i, j - level]
+		C = frame[i + level, j - level]
+		D = frame[i + level, j]
+		E = frame[i, j + level]
+		F = frame[i + level, j + level]
 		return [B,C,D,E,F]
-	elif i != 0 and j == 0 and i != (NUM_ROWS - 1) and j != (NUM_COLS - 1):
-		D = frame[i - 1, j]
-		E = frame[i + 1, j]
-		F = frame[i - 1, j + 1]
-		G = frame[i, j + 1]
-		H = frame[i + 1, j + 1]
+	elif i != min_bd and j == min_bd and i != left_bd and j != right_bd:
+		D = frame[i - level, j]
+		E = frame[i + level, j]
+		F = frame[i - level, j + 1]
+		G = frame[i, j + level]
+		H = frame[i + level, j + level]
 		return [D, E, F, G, H]
-	elif i == 0 and j == 0 and i != (NUM_ROWS - 1) and j != (NUM_COLS - 1):
-		D = frame[i + 1, j]
-		E = frame[i, j + 1]
-		F = frame[i + 1, j + 1]
+	elif i == min_bd and j == min_bd and i != left_bd and j != right_bd:
+		D = frame[i + level, j]
+		E = frame[i, j + level]
+		F = frame[i + level, j + level]
 		return [D, E, F]
-	elif i == (NUM_ROWS - 1) and j != (NUM_COLS - 1):
-		A = frame[i - 1, j - 1]
-		B = frame[i, j - 1]
-		C = frame[i - 1, j]
-		D = frame[i - 1, j + 1]
-		E = frame[i, j + 1]
+	elif i != min_bd and j != min_bd and i == left_bd and j != right_bd:
+		A = frame[i - level, j - level]
+		B = frame[i, j - level]
+		C = frame[i - level, j]
+		D = frame[i - level, j + level]
+		E = frame[i, j + level]
 		return [A, B, C, D, E]
-	elif i != (NUM_ROWS - 1) and j == (NUM_COLS - 1):
-		A = frame[i - 1, j - 1]
-		B = frame[i, j - 1]
-		C = frame[i + 1, j - 1]
-		D = frame[i - 1, j]
-		E = frame[i + 1, j]
+	elif i != min_bd and j != min_bd and i != left_bd and j == right_bd:
+		A = frame[i - level, j - level]
+		B = frame[i, j - level]
+		C = frame[i + level, j - level]
+		D = frame[i - level, j]
+		E = frame[i + level, j]
 		return [A, B, C, D, E]
-	elif i == (NUM_ROWS - 1) and j == (NUM_COLS - 1):
-		A = frame[i - 1, j - 1]
-		B = frame[i, j - 1]
-		D = frame[i - 1, j]
+	elif i != min_bd and j != min_bd and i == left_bd and j == right_bd:
+		A = frame[i - level, j - level]
+		B = frame[i, j - level]
+		C = frame[i - level, j]
+		return [A, B, C]
+	elif i == min_bd and j != min_bd and i != left_bd and j == right_bd:
+		A = frame[i, j - level]
+		B = frame[i + level, j - level]
+		D = frame[i + level, j]
 		return [A, B, D]
+	elif i != min_bd and j == min_bd and i == left_bd and j != right_bd:
+		A = frame[i - level, j]
+		B = frame[i - level, j + level]
+		C = frame[i, j + level]
+		return [A, B, C]
+	else:
+		print "Error in frame neighbor computer."
+
+#Returns neighbors of a pixel at arbitrary radius
+#Neighbors are returned in a circle rather than a square
+def frame_neighbors_n(frame, index_tuple, level=1):
+	if level == 0:
+		return [frame[index_tuple]]
+	if level == 1:
+		return frame_neighbors(frame, index_tuple)
+	masked_frame = mk_circle(frame, index_tuple, level)
+	rv = masked_frame[np.nonzero(masked_frame)]
+	return rv
 
 #Takes numpy array, index of pixel in question, and integer number of standard deviations
 #If pixel in question and its immediate neighbors are n SD outside of the mean, the pixel is considered 'significant'
@@ -108,20 +168,20 @@ def is_significant(frame, index_tuple, deviations = 1, mean = -1, SD = -1):
 	lower_bound = frame_mean + deviations * frame_SD
 	if frame[index_tuple] > lower_bound:
 		neighbors = frame_neighbors(frame, index_tuple)
-		for nbr in frame_neighbors:
-			if frame[nbr] <= lower_bound:
+		for nbr in neighbors:
+			if nbr <= lower_bound:
 				return False
 		return True
 	return False
 
-#Returns data with items outside circle (of given radius) masked by zero
+#Returns data with items not lying on circle (of given radius) masked by zero
 #Uses the Bresenham Circle Algorithm to draw nice looking circles in taxicab geometry
 def mk_circle(frame, center_tuple, radius):
 	(x0, y0) = center_tuple
-	new_frame = np.array([[0]*32]*32)
-	x = radius
-	y = 0
-	radiusError = 1 - x
+	new_frame = np.array([[0.0]*32]*32)
+	x = float(radius)
+	y = 0.0
+	radiusError = 1.0 - x
 	while x >= y:
 		#print new_frame[0,1]
 		new_frame[x+x0,y+y0] = frame[x+x0, y+y0]
@@ -134,10 +194,10 @@ def mk_circle(frame, center_tuple, radius):
 		new_frame[y+x0,-x+y0] = frame[y+x0, -x+y0]
 		y += 1
 		if radiusError < 0:
-			radiusError += 2*y + 1
+			radiusError += 2.0*y + 1.0
 		else:
 			x -= 1
-			radiusError += 2*(y - x + 1)
+			radiusError += 2.0*(y - x + 1.0)
 	return new_frame
 
 #Adaptation of Bresenham Circle Algorithm for disks
@@ -167,10 +227,30 @@ def print_frame(frame, spacing = 1, num_sigfigs = 0):
 				a = len(str(round(element,num_sigfigs)))
 				print str(round(element,num_sigfigs)) + " " * (spacing-a),
 			else:
-				print "0", " " * (spacing-1),
+				print "0", " " * (spacing - 1),
 		print ""
 
-#Returns numerical measurement describing quality of mask
+#Plots light curve with respect to a given pixel as the origin
+#Saves image in images folder
+def light_curve(frame, index_tuple, max_radius = 5):
+	x_points = []
+	y_points = []
+	x = 0
+	for rad in range(max_radius + 1):
+		z = frame_neighbors_n(frame, index_tuple, level = rad)
+		print z
+		avg = (sum(z))/(float(len(z)))
+		x_points.append(x)
+		y_points.append(avg)
+		x += 1
+	plt.plot(x_points, y_points, 'r--')
+	plt.title('Light Curve from pixel ' + str(index_tuple))
+	plt.xlabel('Radius (pixels)')
+	plt.ylabel('Average luminosity')
+	plt.savefig("images/light_curve" + str(index_tuple[0]) + "-" + str(index_tuple[0]) + ".png")
+	plt.show()
+
+#Should return numerical measurement describing quality of mask (in progress)
 def aperture_metric(frame, mask):
 	#Qualities we want in a mask:
 		#Conserves valuable information.
@@ -191,10 +271,13 @@ def main():
 	frame_list = hdulist[0].data
 	frame_one = frame_list[0]
 	
-	#Prints a version of the star with center at brightest pixel, and radius r
-	max_pixel = brightest_pixel(frame_one)
-	test_disk = mk_disk(frame_one, max_pixel, 3)
-	print_frame(test_disk, spacing = 3)
+	#Prints a version of the star with center at top left of brightest region, and radius r
+	max_pixel = brightest_region(frame_one)
+	test_disk = mk_circle(frame_one, (0,0), radius = 2)
+	#print_frame(test_disk, spacing = 2, num_sigfigs = 0)
+	light_curve(frame_one, max_pixel)
+	#print "Max pixel: ",max_pixel
+	#print is_significant(frame_one, max_pixel, deviations = 1)
 	
 	#subprocess.call(["ds9", "-zoom","8", fits_file])
 
